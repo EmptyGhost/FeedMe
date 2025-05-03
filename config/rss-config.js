c5bfca73843f24380f98a3a784f25978ab338f41
EmptyGhost <77950349+EmptyGhost@users.noreply.github.com>
2025-05-03 15:25:27 +0800

    Update rss-config.js

--- config/rss-config.js
+++ config/rss-config.js
@@ -53,11 +53,11 @@ export const config = {
       url: "https://rsshub.rssforever.com/google/research",
       category: "技术博客",
     },
-    // {
-    //   name: "Product Hunt 今日热门",
-    //   url: "https://rsshub.rssforever.com/producthunt/today",
-    //   category: "产品资讯",
-    // },
+    {
+      name: "Product Hunt 今日热门",
+      url: "https://rsshub.app/producthunt/today",
+      category: "产品资讯",
+    },
     {
       name: "LINUX DO 今日热门",
       url: "https://r4l.deno.dev/https://linux.do/top.rss?period=daily",
@@ -74,14 +74,89 @@ export const config = {
       category: "论坛",
     },
     {
-      name: "V2EX 今日热门",
-      url: "https://rsshub.rssforever.com/v2ex/topics/hot",
-      category: "论坛",
+      name: "华尔街日报 - 美国国内新闻",
+      url: "https://feeds.content.dowjones.io/public/rss/RSSUSnews",
+      category: "新闻",
     },
     {
-      name: "Bangumi 近一月热门",
-      url: "https://rsshub.rssforever.com/bangumi.tv/anime/followrank",
-      category: "番剧资讯",
+      name: "华尔街日报 - 美国政治新闻",
+      url: "https://feeds.content.dowjones.io/public/rss/socialpoliticsfeed",
+      category: "新闻",
+    },
+    {
+      name: "华盛顿日报 - 头条新闻",
+      url: "https://www.washingtontimes.com/rss/headlines/news/",
+      category: "新闻",
+    },
+    {
+      name: "华盛顿日报 - 美国国内新闻",
+      url: "https://www.washingtontimes.com/rss/headlines/news/national/",
+      category: "新闻",
+    },
+    {
+      name: "路透社 - 美国新闻",
+      url: "https://rsss.templateapot.cn/reuters/world/us",
+      category: "新闻",
+    },
+    {
+      name: "纽约时报 - 头条新闻",
+      url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+      category: "新闻",
+    },
+    {
+      name: "华盛顿邮报 - 美国国内新闻",
+      url: "https://rsss.templateapot.cn/washingtonpost/app/national",
+      category: "新闻",
+    },
+    {
+      name: "路透社世界新闻 - 中国",
+      url: "https://rsss.templateapot.cn/reuters/world/china",
+      category: "新闻",
+    },
+    {
+      name: "联合早报 - 即时新闻 - 中国",
+      url: "https://rsss.templateapot.cn/zaobao/realtime/china",
+      category: "新闻",
+    },
+    {
+      name: "BBC World News - 亚太区",
+      url: "https://rsss.templateapot.cn/bbc/world-asia",
+      category: "新闻",
+    },
+    {
+      name: "RTHK - 香港新闻",
+      url: "https://rthk.hk/rthk/news/rss/c_expressnews_clocal.xml",
+      category: "新闻",
+    },
+    {
+      name: "CGTN - 国际新闻",
+      url: "https://www.cgtn.com/subscribe/rss/section/world.xml",
+      category: "新闻",
+    },
+    {
+      name: "南华早报",
+      url: "https://www.scmp.com/rss/91/feed",
+      category: "新闻",
+    },
+    {
+      name: "英国《独立报》 - 英国新闻",
+      url: "https://www.independent.co.uk/news/uk/rss",
+      category: "新闻",
+    },
+    {
+      name: "华盛顿邮报 - 美国国内新闻",
+      url: "https://rsss.templateapot.cn/washingtonpost/app/national",
+      category: "新闻",
+    },
+    {
+      name: "CVE漏洞资讯 - Latest CVE Feed",
+      url: "https://cvefeed.io/rssfeed/latest.xml",
+      category: "软件安全",
+    },
+    {
+      name: "CVE漏洞资讯 - High and Critical Severity",
+      url: "https://cvefeed.io/rssfeed/severity/high.xml",
+      category: "软件安全",
     },
   ],
   maxItemsPerFeed: 30,
